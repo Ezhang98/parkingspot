d4c8a315a429c4d3247d432aba50fa1e38ba2a3b
Colin Peppler <ccp2807@email.vccs.edu>
2019-01-11 23:43:22 -0800

    Added the pin button

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+		<View style={styles.pinButton}>
+			<Button
+				onPress = {() => {
+					Alert.alert("Button has been pressed")
+				}}
+				title="Pin"
+				color="#ffffff"
+			/>
+		</View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  	container: {
+   		flex: 1,
+    	backgroundColor: '#fff',
+    	alignItems: 'center',
+    	justifyContent: 'center',
+  	},
+	pinButton: {
+		backgroundColor: '#FF0000',
+		position: 'absolute',
+		bottom: 10,
+	}
+	
 });
