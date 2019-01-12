f13999c95a551a6dbe9eec079db8c8d22cbc7147
Colin Peppler <ccp2807@email.vccs.edu>
2019-01-12 00:22:01 -0800

    Fixed button and mapview

--- App.js
+++ App.js
@@ -3,19 +3,9 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import {MapView} from 'expo';
 
 export default class App extends React.Component {
-/*  render() {
+  render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-		<View style={styles.pinButton}>
-			<Button
-				onPress = {() => {
-					Alert.alert("Button has been pressed")
-				}}
-				title="Pin"
-				color="#ffffff"
-			/>
-		</View>
         <MapView
             style={{ flex: 1 }}
             initialRegion={{
@@ -25,36 +15,17 @@ export default class App extends React.Component {
                 longitudeDelta: 0.0421,
             }}
         />
-      </View>
-      
-    );
-  }*/
-      render() {
-    return (
-        <View>
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        <OverlayComponent
-            <View style={styles.pinButton}>
+		<View style={styles.pinButton}>
 			<Button
 				onPress = {() => {
 					Alert.alert("Button has been pressed")
 				}}
 				title="Pin"
 				color="#ffffff"
-			/>
+			/> 
 		</View>
-        />
-        }}
-      />
-       </View>
-	
-                
+      </View>
+      
     );
   }
 }
@@ -62,14 +33,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   	container: {
    		flex: 1,
-    	//backgroundColor: '#fff',
-    	alignItems: 'center',
-    	justifyContent: 'center',
+    	backgroundColor: '#fff',
+		justifyContent: 'center',
   	},
 	pinButton: {
 		backgroundColor: '#FF0000',
 		position: 'absolute',
 		bottom: 10,
-	}
-	
+		left: '45%',
+	},
 });
+
