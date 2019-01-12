2ade9e61a66830470e9f1c5d6daa896954607612
Colin Peppler <ccp2807@email.vccs.edu>
2019-01-12 10:46:33 -0800

    Added map view to top half of next screen

--- App.js
+++ App.js
@@ -2,26 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, NavigatorIOS } from 'react-native';
 import {MapView} from 'expo';
 import PropTypes from 'prop-types';
-//import { Marker } from 'react-native-maps';
-
-//const Marker = MapView.Marker
+import { GestureHandler } from 'expo';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 export default class App extends React.Component {
   render() {
-    //const { region } = this.props
     return (
 		<View style={styles.container}>  
-        <NavigatorIOS
-        initialRoute={{
-          component: MyScene,
-          title: 'My Initial Scene',
-          passProps: {index: 1},
-        }}
-        style={{flex: 1}}
-        />
-        
-      </View>
-      
+            <NavigatorIOS
+                initialRoute={{
+                    component: MyScene,
+                    title: 'My Location',
+                    passProps: {index: 1},
+                }}
+                style={{flex: 1}}
+            />
+        </View>
     );
   }
 }
@@ -43,40 +39,32 @@ class MyScene extends React.Component {
     let nextIndex = ++this.props.index;
     this.props.navigator.push({
       component: AnotherScene,
-      title: 'Main Scene ' + nextIndex,
+      title: 'Another Scene ',
       passProps: {index: nextIndex},
     });
   }
 
   render() {
     return (
-
 	  <View style={styles.container}>
 		<MapView
 			style={{ flex: 1 }}
-			//region={region}
 			onRegionChange={this.onRegionChange}
 			showsMyLocationButton={true}
 			showsUserLocation={true}
 			followsUserLocation={true}
 		> 
 		</MapView>
-		<View>
-		<Button
-		  onPress={this._onForward}
-		  title="Tap me to load the next scene"
-		/>
-	  	</View>
 		<View style={styles.pinButton}>
 			<Button
-				onPress = {() => {
-					Alert.alert("Button has been pressed")
-				}}
-				title="Pin"
+                onPress={this._onForward}
+				title="Pin Location"
 				color="#ffffff"
 			/> 
 		</View>
+       
 	  </View>
+
     );
   }
 }
@@ -98,7 +86,7 @@ class AnotherScene extends React.Component{
 	let nextIndex = ++this.props.index;
     this.props.navigator.push({
       component: MyScene,
-      title: 'Another Scene ' + nextIndex, 
+      title: 'Main Scene ',
       passProps: {index: nextIndex},
     });
   }
@@ -106,13 +94,21 @@ class AnotherScene extends React.Component{
   render() {
 	  return(
 		<View style={styles.container}>
-			<View style={styles.locationInfo}>
-				<Text> Hello World! </Text>
-			</View>
-			<Button
-				onPress={this._onForward}
-				title="Tap me to go to main screen"
-			/>
+			<MapView
+				style={{ flex: 1 }}
+				onRegionChange={this.onRegionChange}
+				showsMyLocationButton={true}
+				showsUserLocation={true}
+				followsUserLocation={true}
+			> 
+			</MapView>
+				<View style={styles.locationInfo}>
+					<Text> Hello World! </Text>
+				</View>
+				<Button
+					onPress={this._onForward}
+					title="Tap me to go to main screen"
+				/>
 		</View>
 	  );
   }
@@ -126,13 +122,13 @@ const styles = StyleSheet.create({
   	},
 	pinButton: {
 		backgroundColor: '#FF0000',
-		position: 'absolute',
-		bottom: '9%',
-		left: '45%',
+		position: 'relative',
+		bottom: '1%'
 	},
 	locationInfo: {
 		flex: 1,
-		justifyContent: 'center',
+		alignItems: 'center',
+		top: '20%',
 	},
 });
 
