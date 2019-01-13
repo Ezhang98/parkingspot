8afe857d9b9b8276011434b13d0cff1fd9e8e111
Evan Zhang <evanlzhang@gmail.com>
2019-01-12 20:45:30 -0800

    Some tables

--- Saved.js
+++ Saved.js
@@ -1,15 +1,51 @@
 import React from 'React';
 import { MapView } from 'expo';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import storage from 'react-native-modest-storage';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 
 export default class Notes extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          tableHead: ['Head', 'Head2', 'Head3', 'Head4'],
+          tableData: [
+            ['1', '2', '3', '4'],
+            ['a', 'b', 'c', 'd'],
+            ['1', '2', '3', '4'],
+            ['a', 'b', 'c', 'd']
+          ]
+        }
+      }
+
     render() {
-        return(
-            <View style={styles.container}>
+        const state = this.state;
+        const element = (data, index) => (
+          <TouchableOpacity onPress={() => this._alertIndex(index)}>
+            <View style={styles.btn}>
+              <Text style={styles.btnText}>button</Text>
             </View>
+          </TouchableOpacity>
+        );
+        return (
+            <View style={styles.container}>
+            <Table borderStyle={{borderColor: 'transparent'}}>
+              <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+              {
+                state.tableData.map((rowData, index) => (
+                  <TableWrapper key={index} style={styles.row}>
+                    {
+                      rowData.map((cellData, cellIndex) => (
+                        <Cell key={cellIndex} data={cellIndex === 3 ? element(cellData, index) : cellData} textStyle={styles.text}/>
+                      ))
+                    }
+                  </TableWrapper>
+                ))
+              }
+            </Table>
+          </View>
         )
     }
 }
