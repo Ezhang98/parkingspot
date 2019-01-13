9b0104826f2f4ed390d80289c951f80bf5212eee
Evan Zhang <evanlzhang@gmail.com>
2019-01-12 20:53:50 -0800

    almost done

--- Saved.js
+++ Saved.js
@@ -23,11 +23,11 @@ export default class Notes extends React.Component{
     render() {
         const state = this.state;
         const element = (data, index) => (
-          <TouchableOpacity onPress={() => this._alertIndex(index)}>
+
             <View style={styles.btn}>
               <Text style={styles.btnText}>button</Text>
             </View>
-          </TouchableOpacity>
+
         );
         return (
             <View style={styles.container}>
@@ -51,22 +51,10 @@ export default class Notes extends React.Component{
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		justifyContent: 'center',
-	},
-	notes: {
-		flex: 1,
-		paddingTop: '2%',
-		left: '2%',
-	},
-	buttons: {
-		position: 'relative',
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-		paddingTop: '50%',
-		alignItems: 'center',
-	},
+    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+    head: { height: 40, backgroundColor: '#808B97' },
+    text: { margin: 6 },
+    row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+    btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
+    btnText: { textAlign: 'center', color: '#fff' }
 });
